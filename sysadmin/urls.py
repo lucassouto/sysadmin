eea1f3fd50223dcbb607c1eb881204e478f60f79
Lucas Souto <lucasinfologos@gmail.com>
2018-02-23 19:07:26 -0300

    Config django-sysace

---
+++ sysadmin/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'sysadmin.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ace/', include('ace.urls')),
+    url(r'session_security/', include('session_security.urls')),
+]
